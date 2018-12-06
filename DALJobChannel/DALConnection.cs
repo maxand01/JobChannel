ed4497c947e9ccb7f699b6f092110eaa6d285942
maxime roussel <maxand01@hotmail.fr>
2018-12-06 16:10:02 +0100

    Creation DLL DAL + DALConnection + DALException

---
+++ DALJobChannel/DALConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALJobChannel
+{
+    /// <summary>
+    /// Classe Connection
+    /// </summary>
+    public class DALConnection
+    {
+        private static DALConnection _Instance;
+
+        private SqlConnection cn;
+
+        private DALConnection()
+        {
+            try
+            {
+                cn = new SqlConnection();
+                cn.ConnectionString = ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+                cn.Open();
+            }
+            catch (SqlException ex)
+            {
+
+                throw new DALException(Properties.Resources.ERROR_CONNECTION, ex);
+            }
+        }
+    }
+}
---
+++ DALJobChannel/DALException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALJobChannel
+{
+    [Serializable]
+    public class DALException : Exception
+    {
+        public DALException() { }
+        public DALException(string message) : base(message) { }
+        public DALException(string message, Exception inner) : base(message, inner) { }
+        protected DALException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
